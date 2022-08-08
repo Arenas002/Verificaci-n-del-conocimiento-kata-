a35a2fbf5506eda375bee0e460df48a4daba06e1
Arenas <cdarenas02@gmail.com>
2022-08-08 09:24:18 -0500

    se grego el API falsa

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ $form = d.querySelector(".crud-form"),
 $title = d.querySelector(".crud-title"),
 $template= d.getElementById(".crud-form").content,
 $fragmen = d.createElementFragment();
+
+const getAll= async ()=>{
+    try{
+        let res = await fetch()
+    }catch{
+
+    }
+}
+
+
+d.addEventListener("DOMContentLoaded",getAll)
